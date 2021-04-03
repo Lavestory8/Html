13ae9b3957de15e615c3c0792460f204b3b916a2
moody202 <mahmoudabdelhakam89@gmail.com>
2021-04-03 18:01:01 +0300

    Create mahmoud.py

---
+++ mahmoud.py
@@ -0,0 +1,16 @@
+x = 5
+y=4
+r =8
+print(x,y,r)
+
+myAem= [8,5,6]
+myAem.append(9)
+print(myAem)
+
+y = [1,4,5,7,8,3]
+a =["m","h","u"]
+
+print(y+a)
+
+ft = ["my name is mahmoud abdelhakam sayed ayuob"]
+print(type(ft))
